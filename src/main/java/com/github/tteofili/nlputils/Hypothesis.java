d44e49da6d0d0823027f934b99843a71d30a7540
Tommaso Teofili <teofili@adobe.com>
2013-01-02 13:44:34 +0100

    fixed LMS, added #updateWeights to Hypothesis

--- src/main/java/com/github/tteofili/nlputils/Hypothesis.java
+++ src/main/java/com/github/tteofili/nlputils/Hypothesis.java
@@ -4,5 +4,19 @@ package com.github.tteofili.nlputils;
  * An {@link Hypothesis} maps a series of inputs in an output
  */
 public interface Hypothesis {
-    double calculateOutput(double... inputs);
+
+    /**
+     * calculate the output given some inputs according to the underlying model.
+     *
+     * @param inputs an array of inputs as <code>double</code>
+     * @return a <code>double</code> representing the output
+     */
+    public double calculateOutput(double[] inputs);
+
+    /**
+     * update the internal model's parameters.
+     *
+     * @param parameters an array of <code>double</code> containing the updated parameters
+     */
+    public void updateParameters(double[] parameters);
 }
--- src/main/java/com/github/tteofili/nlputils/RegressionModelUtils.java
+++ src/main/java/com/github/tteofili/nlputils/RegressionModelUtils.java
@@ -7,8 +7,9 @@ public class RegressionModelUtils {
 
     /**
      * calculate the ordinary least squares (OLS) cost in the given training set for a given hypothesis
+     *
      * @param trainingSet the training set used
-     * @param hypothesis the hypothesis function representing the model
+     * @param hypothesis  the hypothesis function representing the model
      * @return the cost of the hypothesis for the given training set using OLS
      */
     public static double ordinaryLeastSquares(TrainingSet trainingSet, Hypothesis hypothesis) {
@@ -20,20 +21,24 @@ public class RegressionModelUtils {
     }
 
     /**
-     * calculate the least mean square (LMS) update for a given weight
-     * @param thetas the array of weights
-     * @param alpha the learning rate alpha
+     * calculate the least mean square (LMS) update for a given weight vector
+     *
+     * @param thetas      the array of weights
+     * @param alpha       the learning rate alpha
      * @param trainingSet the training set to use for learning
-     * @param hypothesis the hypothesis representing the model
-     * @param j the index of the weight to update
+     * @param hypothesis  the hypothesis representing the model
      * @return the updated weight for the j-th element of the weights vector
      */
-    public double leastMeanSquareUpdate(double[] thetas, double alpha, TrainingSet trainingSet, Hypothesis hypothesis, int j) {
+    public double[] leastMeanSquareUpdate(double[] thetas, double alpha, TrainingSet trainingSet, Hypothesis hypothesis) {
         double errors = 0;
         for (TrainingExample trainingExample : trainingSet) {
             errors += trainingExample.getOutput() - hypothesis.calculateOutput(trainingExample.getInputs());
         }
-        return thetas[j] + alpha * errors * thetas[j];
+        double[] updatedWeights = new double[thetas.length];
+        for (int i = 0; i < updatedWeights.length; i++) {
+            updatedWeights[i] = thetas[i] + alpha * errors * thetas[i];
+        }
+        return updatedWeights;
     }
 
 }
