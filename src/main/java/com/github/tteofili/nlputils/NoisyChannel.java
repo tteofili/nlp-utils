fc12e1cf68862c373ad061a20984e9409faae506
Tommaso Teofili <teofili@adobe.com>
2013-01-02 12:11:06 +0100

    added abstract noisy channel impl

---
+++ src/main/java/com/github/tteofili/nlputils/NoisyChannel.java
@@ -0,0 +1,26 @@
+package com.github.tteofili.nlputils;
+
+/**
+ * Add javadoc here
+ */
+public abstract class NoisyChannel {
+
+    private String[] dictionary;
+
+    public String findCorrection(String mispelledWord) {
+        Double val = 0d;
+        String correctWord = null;
+        for (String word : dictionary) {
+            Double curVal = calculateLikelihood(mispelledWord, word) * calculatePrior(word);
+            if (curVal > val) {
+                val = curVal;
+                correctWord = word;
+            }
+        }
+        return correctWord;
+    }
+
+    public abstract Double calculatePrior(String word);
+
+    public abstract Double calculateLikelihood(String mispelledWord, String word);
+}
